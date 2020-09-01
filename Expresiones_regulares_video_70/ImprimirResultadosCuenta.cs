d834cdda35159f059c7180189a61ea7aaa337242
israelvargasjimenez <58093163+israelvargasjimenez@users.noreply.github.com>
2020-09-01 20:46:06 +0200

    Expresiones_regulares_I Video 70

---
+++ Expresiones_regulares_video_70/ImprimirResultadosCuenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expresiones_regulares_video_70
+{
+    class ImprimirResultadosCuenta
+    {
+        public static void imprimirResultado(int cuenta, string patron) =>
+        Console.WriteLine($"Se han encontrado {cuenta} coincidencias del patron \"{patron}\"");
+    }
+}
--- Expresiones_regulares_video_70/Program.cs
+++ Expresiones_regulares_video_70/Program.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Text.RegularExpressions;
 
 namespace Expresiones_regulares_video_70
 {
     class Program
     {
-
+        private delegate int DelegadoCoincidencias(MatchCollection coleccion);
         
         static void Main(string[] args)
         {
 
-            string frase = "Mi nombre es \'Israel y mi nº de tfno es" +
-                                   "(+34)91-764-63-41, y my codIIIIIIIIIIIIIgo postal 28760";
+            string frase = "Mi nombre es \'IJsrael y mi nº de tfno es" +
+                                   "(+34)91-764-63-41, y my codIJIgo postal 38760" +
+                                   "(+34)91-764-63-42  (+34)91-764-63-55";
 
             // Expresiones regulares video 70-------------------------
             // son muy potentes pero poco utilizadas
@@ -19,18 +21,32 @@ namespace Expresiones_regulares_video_70
             // Se suelen utilizar para buscar cadenas de caracteres
             // La clase que les da nombre es Regex (regular expresion)
 
-            string pattern1 = @"[I]";
+            string pattern1 = @"IJ";
+
+            string pattern2 = @"(\d{5})";
+
+            string pattern3 = @"\W{2}\d{2}\W\d{2}-\d{3}-\d{2}-\d{2}";
+
 
             Regex miRegex = new Regex(pattern1);
 
-            MatchCollection resultados = miRegex.Matches(frase);
+            MatchCollection resultados = miRegex.Matches(frase);          
 
-            if (resultados.Count > 0) Console.WriteLine(
-                           $"Se han encontrado {resultados.Count} coincidencias");
+            ImprimirResultadosCuenta.imprimirResultado(resultados.Count, pattern1);
 
 
-            
+            miRegex = new Regex(pattern2);
 
+            resultados = miRegex.Matches(frase);
+
+            ImprimirResultadosCuenta.imprimirResultado(resultados.Count, pattern2);
+
+
+            miRegex = new Regex(pattern3);
+
+            resultados = miRegex.Matches(frase);
+
+            ImprimirResultadosCuenta.imprimirResultado(resultados.Count, pattern3);
         }
     }
 }
